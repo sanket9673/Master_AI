1c8a52bab722770c904d209939f5c59398d23c8f
sanket9673 <sanketch9673@gmail.com>
2024-10-06 07:03:41 +0530

    Sample Landing Website

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -17,6 +17,16 @@ const HeroSection = () => {
             <a href="#" className='bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md'>Start for Free</a>
             <a href="#" className='py-3 px-4 mx-3 rounded-md border'>Documentation</a>
         </div>
+        <div className='flex mt-10 justify-center'>
+            <video autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'>
+            <source src={video1} type='video/mp4' />
+            Your Browser doesn't Support .
+            </video>
+            <video autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'>
+            <source src={video2} type='video/mp4' />
+            Your Browser doesn't Support .
+            </video>
+        </div>
     </div>
   )
 }
