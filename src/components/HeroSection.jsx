e4e5bbf1473338c0a7f08008a8d8ddbe874524b1
sanket9673 <sanketch9673@gmail.com>
2024-10-06 20:07:23 +0530

    Updates

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,11 +6,11 @@ const HeroSection = () => {
   return (
     <div className='flex flex-col items-center mt-6 lg:mt-20'>
         <h1 className='text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide'>
-            VirtualR build tools 
+            MaterAI build tools 
             <span className='bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text'> for Developers </span>
         </h1>
         <p className='mt-10 text-lg text-center text-neutral-500 max-w-4xl'>
-            Empower your creativity and bring your VR app ideas to life without our intuitive developemnt tools. Get started today and turn your imagination
+            Empower your creativity and bring your AI ideas to life without our intuitive developemnt tools. Get started today and turn your imagination
             into immersive reality!
         </p>
         <div className="flex justify-center my-10">
