ab346c2587c738713cd5f1e8a63cbb5a6e11fdea
sanket9673 <sanketch9673@gmail.com>
2024-10-06 17:00:21 +0530

    Testimonial Section Updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HeroSection from './components/HeroSection' ;
 import FeatureSection from './components/FeatureSection';
 import WorkFlow from './components/WorkFlow';
 import Price from './components/Price';
+import Testimonials from './components/Testimonials';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <FeatureSection />
       <WorkFlow />
       <Price />
+      <Testimonials />
     </div>
     </>
   ) 
