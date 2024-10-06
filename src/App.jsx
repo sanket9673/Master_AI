642f4e1ad800ad229b499cc4501de6a35ac25bc6
sanket9673 <sanketch9673@gmail.com>
2024-10-06 15:54:37 +0530

    Feature Section Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection' ;
+import FeatureSection from './components/FeatureSection';
 
 function App() {
   return (
     <>
     <Navbar />
     <div className="max-w-7xl mx-auto pt-20 px-6">
-    <HeroSection />
+      <HeroSection />
+      <FeatureSection />
     </div>
     </>
   ) 
