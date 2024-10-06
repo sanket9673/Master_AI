b630d8cf95297095fc6954908e078adb9cf12f67
sanket9673 <sanketch9673@gmail.com>
2024-10-07 04:35:37 +0530

    Footer Updated

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -5,7 +5,7 @@ const FooterSection = () => {
   return (
     <footer className='mt-10 border-t py-10 border-neutral-700'>
       {/* Responsive grid with gap between columns and rows */}
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-8 px-4 sm:px-8 lg:px-20'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-48 px-4 sm:px-8 lg:px-52'>
 
         {/* Resources Column */}
         <div>
